b74d50359b8b13654cde675ff2856f15487d081a
Kevin <grj@bwg.art>
2019-11-19 15:06:14 +0800

    初始化

---
+++ app.js
@@ -0,0 +1,33 @@
+//app.js
+App({
+  count: {
+    url: 'www.ganrongjian.cn'
+  }
+})
+
+//app.json文件中底部tabBar代码
+// "tabBar": {
+// 	"color": "#ffffff",
+// 		"selectedColor": "#d81e06",
+// 			"backgroundColor": "#000000",
+// 				"list": [
+// 					{
+// 						"pagePath": "pages/index/index",
+// 						"text": "首页",
+// 						"iconPath": "/images/index.png",
+// 						"selectedIconPath": "/images/index-selected.png"
+// 					},
+// 					{
+// 						"pagePath": "pages/accumulation/accumulation",
+// 						"text": "积累",
+// 						"iconPath": "/images/accumulation.png",
+// 						"selectedIconPath": "/images/accumulation-selected.png"
+// 					},
+// 					{
+// 						"pagePath": "pages/mine/mine",
+// 						"text": "我的",
+// 						"iconPath": "/images/mine.png",
+// 						"selectedIconPath": "/images/mine-selected.png"
+// 					}
+// 				]
+// },
